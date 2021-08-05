66ba584159ea7c0d8fadc8169ec043f2fa932799
cby3149 <cby3149@outlook.com>
2021-08-05 12:13:45 -0400

    Update API call

--- src/containers/Transaction.tsx
+++ src/containers/Transaction.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { ethers } from "ethers";
 import TxView from "../components/TxView";
 import LoadingView from "../components/LoadingView/LoadingView";
 import useEthRPCStore from "../stores/useEthRPCStore";
@@ -8,8 +7,6 @@ import {
   TransactionReceiptOrNull as ITransactionReceipt,
 } from "@etclabscore/ethereum-json-rpc";
 import APIWatcher from "../api/watcher";
-
-const { Watcher } = require('@eth-optimism/watcher');
 
 interface CrossDomainMessage {
   blockNumber? : any
@@ -36,30 +33,6 @@ export default function TransactionContainer(props: any) {
   const [crossDomainMessage, setCrossDomainMessage] = React.useState<CrossDomainMessage>({ hash: null });
   const apiWatcher = new APIWatcher();
 
-  const L1Web3 = new ethers.providers.JsonRpcProvider(process.env.REACT_APP_L1_NODE_WEB3_URL);
-  const L2Web3 = new ethers.providers.JsonRpcProvider(process.env.REACT_APP_L2_NODE_WEB3_URL);
-  const watcher = new Watcher({
-    l1: {
-      provider: L1Web3,
-      messengerAddress: "0xF10EEfC14eB5b7885Ea9F7A631a21c7a82cf5D76",
-    },
-    l2: {
-      provider: L2Web3,
-      messengerAddress: "0x4200000000000000000000000000000000000007",
-    },
-  });
-
-  const watcherFast = new Watcher({
-    l1: {
-      provider: L1Web3,
-      messengerAddress: "0xF296F4ca6A5725F55EdF1C67F80204871E65F87d",
-    },
-    l2: {
-      provider: L2Web3,
-      messengerAddress: "0x4200000000000000000000000000000000000007",
-    },
-  });
-
   React.useEffect(() => {
     if (!erpc) { return; }
     erpc.eth_getTransactionByHash(hash).then((tx) => {
@@ -79,24 +52,7 @@ export default function TransactionContainer(props: any) {
   React.useEffect(() => {
     if (transaction) {
       apiWatcher.getCrossDomainMessage(transaction.hash).then(res => {
-        if (res.crossDomainMessageFinalize) {
-          let activeWatcher: any
-          if (res.fastRelay) activeWatcher = watcherFast
-          else activeWatcher = watcher
-          activeWatcher.getMessageHashesFromL2Tx(transaction.hash).then((l2ToL1msgHash: any[]) => {
-            activeWatcher.getL1TransactionReceipt(l2ToL1msgHash[0]).then((l1Receipt: any) => {
-              setCrossDomainMessage({ 
-                ...res, l1Hash: l1Receipt.transactionHash, 
-                l1BlockNumber: l1Receipt.blockNumber,
-                l1BlockHash: l1Receipt.blockHash,
-                l1From: l1Receipt.from,
-                l1To: l1Receipt.to
-              })
-            })
-          })
-        } else {
-          setCrossDomainMessage(res)
-        }
+        setCrossDomainMessage(res)
       })
     }
   // eslint-disable-next-line 
